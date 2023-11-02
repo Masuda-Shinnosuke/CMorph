4a2642118efdc63ff95b6a1788d60974b5125158
Masuda-Shinnosuke <masuda@masudashin>
2023-11-02 12:52:18 +0900

    initial commit

---
+++ simulator/src/main/Config/Config.java
@@ -0,0 +1,18 @@
+package simulator.src.main.Config;
+
+public class Config {
+
+    public static final int NUM_OF_SERVERS=3;
+
+    public static final int NUM_OF_DRONE=3;
+
+    public static final double [] SeverXCoordinate = {10.0,30.0,50.0};
+
+    public static final double [] SeverYCoordinate = {10.0,20.0,40.0}; 
+
+    public static final int SimStep = 100; 
+
+    public static final int mapHeight = 300;
+
+    public static final int mapWidth =300;
+}
